38e0f76dc0f21e969c9e6ffc89ad905c268d0bdd
Thomas Leonhardt <thomas.leonhardt@promos-consult.de>
2022-10-11 12:42:41 +0200

    Initial Commit

---
+++ iTuneStoreSearchMusic/iTuneStoreSearchMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTuneStoreSearchMusicApp.swift
+//  iTuneStoreSearchMusic
+//
+//  Created by Thomas Leonhardt on 11.10.22.
+//
+
+import SwiftUI
+
+@main
+struct iTuneStoreSearchMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
